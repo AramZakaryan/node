511e5e0054b39f7d3e9a2989d6bdb484682997a0
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-05-08 21:40:24 +0400

    interim niveau2

--- dist/index.js
+++ dist/index.js
@@ -7,8 +7,26 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 // const port = 3000
+const products = [
+    { title: "tomato" },
+    { title: "orange" }
+];
+const addresses = [
+    { value: "Mira 7" },
+    { value: "Ulentsi 23" },
+];
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+app.get('/products/:name', (req, res) => {
+    const product = products.find(p => p.title === req.params.name);
+    res.send(product);
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,40 @@
 import express, {Request, Response} from 'express'
+
 const app = express()
 const port = process.env.PORT || 3000
 // const port = 3000
+
+
+const products = [
+    {title: "tomato"},
+    {title: "orange"}
+]
+
+const addresses = [
+    {value: "Mira 7"},
+    {value: "Ulentsi 23"},
+]
 
 app.get('/', (req: Request, res: Response) => {
 
     res.send('Hello World!')
 })
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+
+app.get('/products/:name', (req: Request, res: Response) => {
+
+    const product = products.find(p=>p.title===req.params.name)
+    res.send(product)
+})
+
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
